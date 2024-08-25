138e153209ab8b8355a564a987fd5bb3eab4109d
Lizzy Poker Face <165659964+lizzypokerface@users.noreply.github.com>
2024-08-25 11:58:06 +0800

    refactor(main.py): encapsulate steps

--- _scripts/main.py
+++ _scripts/main.py
@@ -16,30 +16,46 @@ from constants import (
 logging.basicConfig(level=logging.INFO)
 
 
-def run():
-    # Ensure files are cleared
+def clear_caches():
+    logging.info("Clearing caches.")
+    system_say("Clearing caches.")
     clear_file(RAW_LINKS_FILENAME)
     clear_file(PROCESSED_LINKS_FILENAME)
     clear_file(NEWS_LINKS_MD_FILENAME)
+    logging.info("Caches cleared.")
+    system_say("Caches cleared.")
 
-    # STEP 1
+
+def step_1_perform_news_research():
     logging.info("Starting news research.")
     system_say("Starting news research.")
     perform_news_research(NEWS_SOURCES_FILENAME)
     logging.info("News research completed.")
     system_say("News research completed.")
-    # STEP 2
+
+
+def step_2_process_markdown_links():
     logging.info("Processing markdown links.")
     system_say("Processing markdown links.")
     process_markdown_links(PROCESSED_LINKS_FILENAME, NEWS_LINKS_MD_FILENAME)
     logging.info("Markdown link processing completed.")
     system_say("Markdown link processing completed.")
-    # STEP 3
+
+
+def step_3_create_weekly_post_template():
     logging.info("Creating weekly news post template.")
     system_say("Creating weekly news post template.")
     create_weekly_news_post_template(current_date=datetime.now())
     logging.info("Weekly news post template created.")
     system_say("Weekly news post template created.")
+
+
+def run():
+
+    clear_caches()
+    step_1_perform_news_research()
+    step_2_process_markdown_links()
+    step_3_create_weekly_post_template()
 
 
 if __name__ == "__main__":
